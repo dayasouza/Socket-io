6aec3ccda158717dc90e3051a45b1bbf277df7dc
dayasouza <dayanalvesouza@gmail.com>
2025-07-10 19:18:48 -0300

    Feita autenticação em pagina

--- public/documento/socket-front-documento.js
+++ public/documento/socket-front-documento.js
@@ -1,12 +1,26 @@
-import { alertarERedirecionar, atualizaTextoEditor } from "./documento.js";
+import { alertarERedirecionar, atualizarInterfaceUsuarios, atualizaTextoEditor, tratarAutorizacaoSucesso } from "./documento.js";
+import { obterCookie } from "../utils/cookies.js";
 
-const socket = io();
+const socket = io("/usuarios", {
+  auth: {
+    token: obterCookie("tokenJwt"),
+  }
+});
 
-function selecionarDocumento(nome) {
-  socket.emit("selecionar_documento", nome, (texto) => {
+socket.on("autorizacao_sucesso", tratarAutorizacaoSucesso);
+
+socket.on("connect_error", (erro) => {
+  alert(erro);
+  window.location.href = "/login/index.html";
+});
+
+function selecionarDocumento(dadosEntrada) {
+  socket.emit("selecionar_documento", dadosEntrada, (texto) => {
     atualizaTextoEditor(texto);
   });
 }
+
+socket.on("usuarios_no_documento", atualizarInterfaceUsuarios);
 
 function emitirTextoEditor(dados) {
   socket.emit("texto_editor", dados);
