fc4fc3b1d4d4bfe2524daf68bc789a5b17cbcc1a
dayasouza <dayanalvesouza@gmail.com>
2025-07-10 12:27:41 -0300

    Commit inicial

---
+++ servidor/socket-back.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import registrarEventosDocumento from "./registrarEventos/Documento.js";
+import registrarEventosInicio from "./registrarEventos/Inicio.js";
+import registrarEventosCadastro from "./registrarEventos/Cadastro.js";
+import registrarEventosLogin from "./registrarEventos/Login.js";
+import io from "./servidor.js";
+
+io.on("connection", (socket) => {
+  registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket, io);
+  registrarEventosInicio(socket, io);
+  registrarEventosDocumento(socket, io);
+});
